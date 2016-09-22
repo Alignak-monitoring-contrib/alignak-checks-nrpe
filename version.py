3b67dfda51d5d1b63341b2437bff47d51acb21c3
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-09-22 21:32:56 +0200

    Update with last Alignak setup / configuration

--- version.py
+++ version.py
@@ -9,19 +9,19 @@
     Alignak - Checks pack for monitoring hosts with NRPE
 """
 # Package name
-__pkg_name__ = "alignak_checks_nrpe"
+__pkg_name__ = u"alignak_checks_nrpe"
 
 # Checks types for PyPI keywords
 # Used for:
 # - PyPI keywords
 # - directory where to store files in the Alignak configuration (eg. arbiter/packs/checks_type)
-__checks_type__ = "nrpe"
+__checks_type__ = u"nrpe"
 
 # Application manifest
 __version__ = u"0.2.1"
 __author__ = u"Frédéric MOHIER"
-__author_email__ = "frederic.mohier@gmail.com"
+__author_email__ = u"frederic.mohier@gmail.com"
 __copyright__ = u"(c) 2015 - %s" % __author__
 __license__ = u"GNU Affero General Public License, version 3"
 __url__ = u"https://github.com/Alignak-monitoring-contrib/alignak-checks-nrpe"
-__description__ = "Alignak checks pack for NRPE monitored hosts"
+__description__ = u"Alignak checks pack for NRPE monitored hosts"
